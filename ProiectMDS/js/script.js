509beaa4ac12bf062d65c82b196a07138d69d3b9
Tavy7 <48602187+Tavy7@users.noreply.github.com>
2020-04-13 19:15:05 +0300

    Update script.js

--- ProiectMDS/js/script.js
+++ ProiectMDS/js/script.js
@@ -42,16 +42,4 @@ function check() {
     }
 }
 
-function deleteParentElement (){
-	var icon = document.getElementsByClassName("fal fa-trash-alt delete-task")
-	
-	for (var i = 0; i < icon.length; i++) {
-	  	icon[i].addEventListener('click', function(e) {
-			e.currentTarget.parentNode.remove();
-			}, false);
-	}
-}
 
-window.onload = function(){
-	deleteParentElement()//butoanele de trash can la apasare sterg div-ul parinte
-}

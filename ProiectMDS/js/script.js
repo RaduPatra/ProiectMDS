53fceb8b891036976c890fff96a8193c2db3bf33
RaduPatra <48520340+RaduPatra@users.noreply.github.com>
2020-04-13 19:14:19 +0300

    Update script.js

--- ProiectMDS/js/script.js
+++ ProiectMDS/js/script.js
@@ -1,3 +1,47 @@
+const tasks = document.querySelector('.tasks');
+var inputValue = document.querySelector('.todoinput');
+const add = document.querySelector('.createtodo');
+
+class item {
+    constructor(name) {
+        this.createItem(name);
+    }
+    createItem(name) {
+        var task = document.createElement('div');
+        var check = document.createElement('i');
+        var input = document.createElement('p');
+        var remove = document.createElement('i');
+
+        task.classList.add('task');
+        input.textContent = name;
+
+        input.classList.add('text');
+        check.classList.add("far", "fa-circle", "unchecked");
+        remove.classList.add("fal", "fa-trash-alt", "delete-task");
+        remove.addEventListener('click', () => this.deletetask());
+
+        tasks.appendChild(task);
+        task.appendChild(check);
+        task.appendChild(input);
+        task.appendChild(remove);
+    }
+
+}
+
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.which == 13){
+		check();
+	}
+})
+
+function check() {
+    if (inputValue.value != "") {
+        new item(inputValue.value);
+        inputValue.value = "";
+    }
+}
+
 function deleteParentElement (){
 	var icon = document.getElementsByClassName("fal fa-trash-alt delete-task")
 	

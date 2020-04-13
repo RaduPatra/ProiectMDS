1426cbfa5f093ed09ee5e3ade08df147c84e7d31
Tavy7 <48602187+Tavy7@users.noreply.github.com>
2020-04-13 19:27:05 +0300

    Update script.js

--- ProiectMDS/js/script.js
+++ ProiectMDS/js/script.js
@@ -1,7 +1,3 @@
-const tasks = document.querySelector('.tasks');
-var inputValue = document.querySelector('.todoinput');
-const add = document.querySelector('.createtodo');
-
 class item {
     constructor(name) {
         this.createItem(name);
@@ -18,30 +14,14 @@ class item {
         input.classList.add('text');
         check.classList.add("far", "fa-circle", "unchecked");
         remove.classList.add("fal", "fa-trash-alt", "delete-task");
-
-
-        remove.addEventListener('click', function (e) {
-            e.currentTarget.parentNode.remove();
-            }, false);
-
+        remove.addEventListener('click', function(e) {
+			e.currentTarget.parentNode.remove();
+		  }, false);
 
         tasks.appendChild(task);
         task.appendChild(check);
         task.appendChild(input);
         task.appendChild(remove);
     }
-}
 
-add.addEventListener('click', check);
-window.addEventListener('keydown', (e) => {
-    if (e.which == 13) {
-        check();
-    }
-})
-
-function check() {
-    if (inputValue.value != "") {
-        new item(inputValue.value);
-        inputValue.value = "";
-    }
 }

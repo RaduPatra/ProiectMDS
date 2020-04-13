a5d36809436e6a19d928bea9342d4bb006f5961e
Tavy7 <48602187+Tavy7@users.noreply.github.com>
2020-04-13 18:50:11 +0300

    Update script.js

--- ProiectMDS/js/script.js
+++ ProiectMDS/js/script.js
@@ -1,10 +1,10 @@
 function deleteParentElement (){
 	var icon = document.getElementsByClassName("fal fa-trash-alt delete-task")
-
+	
 	for (var i = 0; i < icon.length; i++) {
 	  	icon[i].addEventListener('click', function(e) {
-		    e.currentTarget.parentNode.remove();
-	 		}, false);
+			e.currentTarget.parentNode.remove();
+			}, false);
 	}
 }
 

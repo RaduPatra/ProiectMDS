f0fe478b89f51decd970ca4c80bf3fd8d5f3df35
Tavy7 <48602187+Tavy7@users.noreply.github.com>
2020-04-13 19:33:26 +0300

    Update script.js

--- ProiectMDS/js/script.js
+++ ProiectMDS/js/script.js
@@ -1,3 +1,7 @@
+const tasks = document.querySelector('.tasks');
+var inputValue = document.querySelector('.todoinput');
+const add = document.querySelector('.createtodo');
+
 class item {
     constructor(name) {
         this.createItem(name);
@@ -25,3 +29,30 @@ class item {
     }
 
 }
+
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.which == 13){
+		check();
+	}
+})
+
+function check() {
+    if (inputValue.value != "") {
+        new item(inputValue.value);
+        inputValue.value = "";
+    }
+}
+
+
+function deleteParentElement (){
+    var icon = document.getElementsByClassName("fal fa-trash-alt delete-task")
+
+    for (var i = 0; i < icon.length; i++) {
+      icon[i].addEventListener('click', function(e) {
+    e.currentTarget.parentNode.remove();
+  }, false);
+}
+}
+
+deleteParentElement();

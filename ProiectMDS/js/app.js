b2a46bd0d76946edd570b0ae2a869fa2da7388b2
Predescu Eduard-Alexandru <61836786+Gashparz@users.noreply.github.com>
2020-06-15 15:04:56 +0300

    Update app.js

--- ProiectMDS/js/app.js
+++ ProiectMDS/js/app.js
@@ -8,6 +8,7 @@ var workSpacesCount = 1; // = 1 doar ca momentan nu este implementare pt local s
 var current_workspace = 1
 var workspaces = []
 
+//Aici se face citirea din localStorage
 var displaymethod = window.localStorage.getItem("displaymethod");
 var todos = window.localStorage.getItem("todos");
 
@@ -21,6 +22,7 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 } 
 
+//Functia searchObj are ca scop cautarea unui Task dupa numele sau in array-ul de Task-uri. Daca se gaseste este returnat indicele, iar daca nu se intoarce -1
 function searchObj(nume) {
     for (i = 0; i < todos.length; i++) {
         if (todos[i].name == nume) {
@@ -30,7 +32,9 @@ function searchObj(nume) {
     return -1;
 }
 
-//tasks trb sa fie sortate descr dupa nr de stele
+
+//Functia are ca scop intoarcerea ultimului task in care a fost activata functia "favorite"
+//tasks trebuie sa fie sortate descr dupa numarul de stele
 function findLastStar() {
     for (i = 0; i < todos.length; i++) {
         if (todos[i].star_flag == 0) {
@@ -40,12 +44,12 @@ function findLastStar() {
     return todos.length;
 }
 
-
+//Folosim addTimeEvent pentru a adauga butonul de alarma si ca acesta sa se modifica cand trece dintr-o stare in alta
 function addTimeEvent(date, i) {
 
     date.addEventListener('click', function (e) {
         if (date.classList.contains("fal")) {
-            //daca e inactiv
+            //Daca butonul de alarma este inactiv
             date.classList.remove("fal")
             date.classList.add("fas")
 
@@ -55,7 +59,7 @@ function addTimeEvent(date, i) {
         }
 
         else if (date.classList.contains("fas")) {
-            //daca e activ
+            //Daca butonul de alarma este activ
             for (i of document.getElementsByClassName("countdown"))
                 i.style.display = "none"
 
@@ -64,7 +68,7 @@ function addTimeEvent(date, i) {
         }
 
         else if (date.classList.contains("far")) {
-            //expirat
+            //Daca butonul de alarma este "expirat"
             date.classList.remove("far", "fa-alarm-exclamation")
             date.classList.add("fal", "fa-alarm-clock")
         }
@@ -73,6 +77,7 @@ function addTimeEvent(date, i) {
     return date
 }
 
+//Rolul functiei getInputObject este de a crea un "input" pentru countdown-ul de la alarma. Acesta modifica alarma intr-un input unde se introduce numarul de minute.
 function getInputObject(i) {
     var inputMinute = document.createElement("INPUT");
     inputMinute.setAttribute("type", "number");
@@ -83,7 +88,7 @@ function getInputObject(i) {
 
 
 
-
+//Cand se apasa tasta "ENTER" se inregistreaza inputul, parsam minutele si secundele, facem countdown si revenim la icon-urile alarmei. 
     inputMinute.addEventListener('keydown', (e) => {
         if (e.which == 13) {
             if (e.currentTarget.value != "") {
@@ -98,6 +103,7 @@ function getInputObject(i) {
                     var now = new Date()
                     var valoare = ((val * 60) - parseInt((now - d) / 1000))//numar secunde
 
+                    //parse time
                     let mins = parseInt(valoare / 60)
                     let secs = parseInt(valoare % 60)
                     let string = mins + ":" + secs
@@ -108,10 +114,11 @@ function getInputObject(i) {
                        clearInterval(x)
 
                         cnt[i].style.display = "none"
+                        //schimbam incon pentru alarma
                         var ceas = document.getElementsByClassName("alarma")
                         ceas[i].classList.remove("fas", "fa-alarm-clock")
                         ceas[i].classList.add("far", "fa-alarm-exclamation")
-
+                        //Cand ajunge la 0 cu timer-ul pornim un sunet de alarma si primim o alerta
                         if (valoare < 0){
                             alert("Countdown over!")
                             var jador = new Audio('/audio/beep.wav');
@@ -128,6 +135,7 @@ function getInputObject(i) {
     return inputMinute
 }
 
+//Functia sort-eaza prima data in localStorage, dupa schimba metoda de inserare si le reinsereaza sortate cand se da click pe buton
 sortByFav.addEventListener('click', e => {
     //sory by stars, update storage
     todos.sort(function (a, b) { return b.star_flag - a.star_flag; });
@@ -149,6 +157,8 @@ sortByFav.addEventListener('click', e => {
     displaymethod = displayMethodAux;
 })
 
+
+//Functia are aceasi functionalitate ca si sortByFav doar ca face sort dupa data in local storage, dupa schimba inserarea si reinsereaza task urile in ordinea sortata
 sortByDate.addEventListener('click', e => {
     //sory by time, update storage
     todos.sort(function (a, b) { return b.creation_date - a.creation_date; });
@@ -170,8 +180,10 @@ sortByDate.addEventListener('click', e => {
     displaymethod = displayMethodAux;
 })
 
+//Clasa principala de care se foloseste tot proiectul. Un obiect de tip item este un task
 class item {
     constructor(name, f1 = 0, f2 = 0, f3 = 0, f4 = 0) {
+        //Avem un constructor pentru nume si flag uri. Mereu cand se apeleaza constr apelam si functia createItem
         this.name = name;
         this.check_flag = f1;
         this.star_flag = f2;
@@ -186,7 +198,7 @@ class item {
 
         this.createItem(name, f1, f2, f3);
     }
-
+//createItem creaza div-ul pentru task, icon-urile pentru butoanele de alarma, favorite, stergere si check si butonul de editare a task-ului si adauga stilizare acestora
     createItem(name, f1, f2, f3) {
         var task = document.createElement('div');
         var dropdowndiv = document.createElement('div');
@@ -201,7 +213,7 @@ class item {
 
         editbtn.classList.add("edit-task", "fa", "fa-edit");
         task.classList.add('task');
-
+        //textul afisat vine de la constr
         input.textContent = name;
         input.classList.add('text');
         input.contentEditable = "false";
@@ -231,17 +243,21 @@ class item {
             tasks.insertBefore(task, tasks.firstChild);
         }
 
+         //dam append diverselor icons si btn in div-ul taskului 
         task.appendChild(check);
         task.appendChild(input);
         task.appendChild(star);
         task.appendChild(dropdowndiv);
-
+        //append pt dropdown
         dropdowndiv.appendChild(options);
         dropdowndiv.appendChild(dropdowncontent);
         dropdowncontent.appendChild(remove);
         dropdowncontent.appendChild(editbtn);
+                //De aici incepe drag, drop and swap folosind .draggable si .droppable din jquery
+        //Este comentat in intregime deoarece avem un bug major si nu am reusit sa il rezolvam in timp util
         // $(document).ready(function (){
         //     $( function() {
+        //pentru un element de tip task pe care dam click va primi tag ul draggable
         //         $( task ).draggable({
         //                 axis: "y",
         //                 containment: "parent",
@@ -257,12 +273,14 @@ class item {
         //         $( task).droppable({
         //                 accept: ".task",
         //                 drop: function(event, ui) {
+                            //luam pozitia taskului draggable si a celui droppable si le interschimbam
+                            //extragem pozitiile
         //                     var draggable = ui.draggable;
         //                     var droppable = $(this);
         //                     var dragPos = draggable.position();
         //                     var dropPos = droppable.position();
         //                     console.log(dragPos + "drop:" + dropPos)
-
+                                //facem interschimbarea
         //                     draggable.css({
         //                         left: dropPos.left + "px",
         //                         top: dropPos.top + "px",
@@ -274,15 +292,14 @@ class item {
         //                 }
         //         })
         //     } );
-
-
         // })
-
+        //cream un div secund pentru alarma folosind functia creareDivInput
         var divSecund = this.creareDivInput(f3, name, dropdowncontent);
         task.appendChild(divSecund);
 
     }
 
+    //creem un div nou pentru input ul de la alarma
     creareDivInput(flag, name, dropdowncontent) {
         var div = document.createElement("div")
 
@@ -305,6 +322,7 @@ class item {
             date.classList.add("far", "fa-alarm-exclamation");
         }
 
+        //ii dam append la div si il returnam
         dropdowncontent.appendChild(addTimeEvent(date, searchObj(name)))
         console.log(div);
         div.appendChild(getInputObject(searchObj(name)))
@@ -316,8 +334,9 @@ class item {
         return div
     }
 
-
+    //adauga functionalitate butonului de check(cel care da crossout unui task)
     checkIcon(check, input, obj, flag) {
+            //Initializare pentru check si uncheck
         if (flag == 1) {
             check.classList.add("far", "fa-check-circle");
             input.style.cssText = "text-decoration:line-through ; color:gray; opacity: 60%";
@@ -325,13 +344,14 @@ class item {
         else {
             check.classList.add("far", "fa-circle", "unchecked");
         }
+         //Un efect de hover 
         check.addEventListener('mouseover', function (e) {
             if (!e.currentTarget.classList.contains("fa-check-circle")) {
                 e.currentTarget.classList.remove("fa-circle", "unchecked");
                 e.currentTarget.classList.add("fa-dot-circle");
             }
         }, false);
-
+        //reverse la efectul de hover
         check.addEventListener("mouseout", function (e) {
             if (!e.currentTarget.classList.contains("fa-check-circle")) {
                 e.currentTarget.classList.remove("fa-dot-circle");
@@ -342,6 +362,7 @@ class item {
         check.addEventListener('click', function (e) {
             //da check
             var defaultstyle = input.style;
+            //Cand se da click pe butonul de check ii dam crossout si il stilizam
             if (e.currentTarget.classList.contains("fa-dot-circle")) {
                 e.currentTarget.classList.remove("fa-dot-circle");
                 e.currentTarget.classList.add("fa-check-circle");
@@ -349,7 +370,7 @@ class item {
                 obj.check_flag = 1;
             }
             else {
-                //nu da check
+                //Cand dam uncheck dam revert la ce am facut mai sus
                 e.currentTarget.classList.remove("fa-check-circle");
                 e.currentTarget.classList.add("fa-circle", "unchecked");
                 input.style.removeProperty("text-decoration", "line-through");
@@ -359,13 +380,14 @@ class item {
             //local storage    
             var ind = searchObj(obj.name);
             todos[ind] = obj;
+            //local storage  
             window.localStorage.setItem("todos", JSON.stringify(todos));
         }, false);
 
         return check;
     }
 
-
+    //Functia editTask are ca scop adaugare functionalitatii pentru butonul de edit. Cand apasam btn textul devine un input pe care putem sa il modificam
     editTask(editbtn, name, input) {
 
         editbtn.addEventListener("click", function (e) {
@@ -375,9 +397,11 @@ class item {
                     event.preventDefault();
                 }
             });*/
+            //cautam task-ul pe care dorim sa il modificam in lista de task-uri
             var ind = searchObj(name);
 
             if (input.contentEditable == "false") {
+                //daca nu a fost inca apasat butonul intram in modul de edit
                 input.contentEditable = "true";
                 input.focus();
                 editbtn.classList.remove("edit-task", "fa", "fa-edit");
@@ -386,16 +410,19 @@ class item {
                 //save on enter
                 input.addEventListener("keypress", function (event) {
                     if (event.keyCode == 13) {
+                        //revenim la forma initiala a task-ului
                         input.contentEditable = "false"
                         editbtn.classList.add("edit-task", "fa", "fa-edit");
                         editbtn.classList.remove("save-edit", "fal", "fa-clipboard-check");
-
+                        //il modificam in vectorul de task-uri 
                         todos[ind].name = input.textContent;
+                        //cat si in localStorage
                         window.localStorage.setItem("todos", JSON.stringify(todos));
                     }
                 });
             }
             else {
+                //Daca mai apasam inca o data pe buton salvam modificarile facute si in localStorage
                 input.contentEditable = "false";
                 editbtn.classList.add("edit-task", "fa", "fa-edit");
                 editbtn.classList.remove("save-edit", "fal", "fa-clipboard-check");
@@ -407,22 +434,23 @@ class item {
 
         return editbtn;
     }
-
+    //Initializeaza behavior-ul icon-ului 
     removeIcon(remove, name, task) {
         remove.classList.add("fal", "fa-trash-alt", "delete-task");
         remove.addEventListener("click", function (e) {
+        //cand se da click se sterge din array ul de task uri 
             console.log(e.currentTarget.parentNode);
             task.remove();
             let ind = searchObj(name);
             todos.splice(ind, 1);
-
+            //si din localstorage
             window.localStorage.setItem("todos", JSON.stringify(todos));
         }, false);
 
         return remove;
     }
 
-
+    //Initializeaza behavior-ul meniului nou adaugat de optiuni-ului 
     optionIcon(options, dropdowncontent, name) {
         options.classList.add("options", "far", "fa-ellipsis-v");
         options.addEventListener("click", function (e) {
@@ -434,7 +462,9 @@ class item {
         return options;
     }
 
+    //initializez behavior-ul butonului de fav
     starIcon(star, obj, flag) {
+         //daca este deja activat il schimb cu cel neactivat si vice versa
         if (flag == 1) {
             star.classList.add("fas", "fa-star")
         }
@@ -490,6 +520,7 @@ class item {
     }
 }
 
+//Functia adauga un task nou apland constructorul de la clasa Item
 function addTodo() {
 
     if (inputValue.value != "") {
@@ -498,7 +529,7 @@ function addTodo() {
             alert("Exista deja acest To-Do!");
             return;
         }
-
+         //apeleaza constr de la clasa item
         var x = new item(inputValue.value);
         inputValue.value = "";
 
@@ -516,24 +547,26 @@ function addTodo() {
         if (displaymethod == 2) {
             todos.unshift(x);
         }
-
+        //adauga in local storage
         window.localStorage.setItem("todos", JSON.stringify(todos));
     }
 }
 
-
+//functia de prelucrare a datelor din local storage. 
 function loadLocalStorage() {
     //localStorage.clear()
 
+    //cand le scoatem din local storage
+    //daca nu au fost citite bine reinitializam displaymethod pentru sortari
     if (displaymethod == null) {
         displaymethod = 2;
         window.localStorage.setItem("displaymethod", displaymethod)
     }
 
     displayMethodAux = displaymethod;
-
+    //ii dam parse
     todos = JSON.parse(todos);
-
+    //apelam constructorul pentru fiecare item din localStorage
     if (todos) {
         displaymethod = 0;
         for (let i = 0; i < todos.length; i++) {
@@ -652,30 +685,35 @@ function initFirstWorkSpace(){
 	//TO DO de adaugat event pt hover 
 }
 
-deleteAllBtn.addEventListener("mouseover", function (e) {
-    e.currentTarget.classList.remove("far")
-    e.currentTarget.classList.add("fas")
-})
+// deleteAllBtn.addEventListener("mouseover", function (e) {
+//     e.currentTarget.classList.remove("far")
+//     e.currentTarget.classList.add("fas")
+// })
 
-deleteAllBtn.addEventListener("mouseout", function (e) {
-    e.currentTarget.classList.remove("fas")
-    e.currentTarget.classList.add("far")
-})
+// deleteAllBtn.addEventListener("mouseout", function (e) {
+//     e.currentTarget.classList.remove("fas")
+//     e.currentTarget.classList.add("far")
+// })
 
+
+//cand se apasa butonul de delte all stergem tot din vectorul de task uri si din localStorage
 deleteAllBtn.addEventListener("click", function (e) {
     localStorage.clear()
     todos = []
     tasks.innerHTML = '';
 })
 
-
+//Functia main
 function main() {
     for (i = 0; i < 30; i++){
     	workspaces[i] = []
     }
+    //apelam functia de prelucrare a datelor din localstorage
     loadLocalStorage();
-    initFirstWorkSpace();
-	
+    initFirstWorkSpace();//cat si a workspace-ului
+    
+    
+    //cand se insereaza textul si se apasa "ENTER" se apeleaza functia de adaugare a unui task nou
     window.addEventListener('keydown', (e) => {
         if (e.which == 13) {
             addTodo();
